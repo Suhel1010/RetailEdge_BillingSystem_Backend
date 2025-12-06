74292c687f7c0fd147c0d351fc125577f0352060
Suhel1010 <suhelansari2018123@gmail.com>
2025-12-07 04:06:36 +0530

    paymenyt section

---
+++ src/main/java/org/billing/io/PaymentVarificationRequest.java
@@ -0,0 +1,19 @@
+package org.billing.io;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentVarificationRequest {
+
+    private String razorpayOrderId;
+    private String razorpayPaymentId;
+    private String razorPaySignature;
+    private String orderId;
+
+
+
+}
--- src/main/java/org/billing/service/impl/OrderServiceImpl.java
+++ src/main/java/org/billing/service/impl/OrderServiceImpl.java
@@ -3,10 +3,7 @@ package org.billing.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.billing.entity.OrderEntity;
 import org.billing.entity.OrderItemEntity;
-import org.billing.io.OrderRequest;
-import org.billing.io.OrderResponse;
-import org.billing.io.PaymentDetails;
-import org.billing.io.PaymentMethod;
+import org.billing.io.*;
 import org.billing.repository.OrderEntityRepository;
 import org.billing.service.OrderService;
 import org.springframework.stereotype.Service;
@@ -91,4 +88,29 @@ public class OrderServiceImpl implements OrderService {
         return orderEntityRepository.findAllByOrderByCreatedAtDesc().stream()
                 .map(this::convertToResponse).collect(Collectors.toList());
     }
+
+    @Override
+    public OrderResponse varifyPayment(PaymentVarificationRequest request) {
+        OrderEntity existingOrder = orderEntityRepository.findByOrderId(request.getOrderId())
+                .orElseThrow(()-> new RuntimeException("Order not found !!") );
+        if(!verifyRazorpaySignature(request.getRazorpayPaymentId(),
+                request.getRazorpayOrderId(),
+                request.getRazorPaySignature())){
+            throw new RuntimeException("Payment varification failed !");
+        }
+
+        PaymentDetails paymentDetails = existingOrder.getPaymentDetails();
+        paymentDetails.setRazorPayOrderId(request.getRazorpayOrderId());
+        paymentDetails.setRazorPayPaymentId(request.getRazorpayPaymentId());
+        paymentDetails.setRazorPaySignature(request.getRazorPaySignature());
+        paymentDetails.setPaymentStatus(PaymentDetails.PaymentStatus.Completed);
+        existingOrder = orderEntityRepository.save(existingOrder);
+        return convertToResponse(existingOrder);
+    }
+
+    private boolean verifyRazorpaySignature(String razorpayPaymentId, String razorpayOrderId, String razorPaySignature) {
+        return true;
+    }
+
+
 }
